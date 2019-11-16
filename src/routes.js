8c26b1810c273b1c9b7d448c01d918e1b668da5c
Omoniyi Omotoso <omoniyiomotoso@Omoniyis-MacBook-Pro.local>
2019-11-16 15:13:06 +0100

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,55 @@
+
+import Dashboard from "views/Dashboard.jsx";
+import UserProfile from "views/UserProfile.jsx";
+import TableList from "views/TableList.jsx";
+import Typography from "views/Typography.jsx";
+import Icons from "views/Icons.jsx";
+import Notifications from "views/Notifications.jsx";
+import Upgrade from "views/Upgrade.jsx";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "pe-7s-graph",
+    component: Dashboard,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "Cloud Config",
+    icon: "pe-7s-user",
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Supported Clouds",
+    icon: "pe-7s-note2",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Devices",
+    icon: "pe-7s-news-paper",
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: "pe-7s-science",
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "pe-7s-bell",
+    component: Notifications,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
